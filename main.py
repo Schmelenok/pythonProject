549f6052a0fe44120d507d295af8222a76b1e3a5
Shmel <shmel_bush@tut.by>
2022-03-09 12:17:02 +0100

    No changes

---
+++ main.py
@@ -0,0 +1,13 @@
+def compare_by_second(point):
+    return point[1]
+
+
+def compare_by_sum(point):
+    return point[0] + point[1]
+
+
+
+points = [(1, -1), (2, 3), (-10, 15), (10, 9), (7, 18), (1, 5), (2, -4)]
+
+print(sorted(points, key=compare_by_second))   # сортируем по второму значению кортежа
+print(sorted(points, key=compare_by_sum))      # сортируем по сумме кортежа
