919b14640be1a0d2392d7467e427d8ac1209971f
Shmel <shmel_bush@tut.by>
2022-03-10 18:30:28 +0100

    something were added

--- main.py
+++ main.py
@@ -135,3 +135,72 @@
 # filtered_numbers = filter(divide7, numbers)
 # map_numbers = map(twice,filtered_numbers)
 # print(sum(map_numbers))
+
+# from functools import reduce
+# floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+# words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+# numbers = [4, 6, 9, 23, 5]
+# # Исправьте этот код
+# map_result = list(map(lambda num: round(num**2,1), floats))
+# filter_result = list(filter(lambda name: name[:] == name[::-1] and len(name) > 4, words))
+# reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
+# print(map_result)
+# print(filter_result)
+# print(reduce_result)
+
+# from functools import reduce
+#
+# data = [['Tokyo', 35676000, 'primary'],
+#         ['New York', 19354922, 'nan'],
+#         ['Mexico City', 19028000, 'primary'],
+#         ['Mumbai', 18978000, 'admin'],
+#         ['Sao Paulo', 18845000, 'admin'],
+#         ['Delhi', 15926000, 'admin'],
+#         ['Shanghai', 14987000, 'admin'],
+#         ['Kolkata', 14787000, 'admin'],
+#         ['Los Angeles', 12815475, 'nan'],
+#         ['Dhaka', 12797394, 'primary'],
+#         ['Buenos Aires', 12795000, 'primary'],
+#         ['Karachi', 12130000, 'admin'],
+#         ['Cairo', 11893000, 'primary'],
+#         ['Rio de Janeiro', 11748000, 'admin'],
+#         ['Osaka', 11294000, 'admin'],
+#         ['Beijing', 11106000, 'primary'],
+#         ['Manila', 11100000, 'primary'],
+#         ['Moscow', 10452000, 'primary'],
+#         ['Istanbul', 10061000, 'admin'],
+#         ['Paris', 9904000, 'primary']]
+# data_filtered = filter(lambda x: x[1] > 10000000 and x[2] == 'primary', data)
+# data_fil_sort = sorted(data_filtered)
+# data_fil_sort_map = map(lambda x: x[0], data_fil_sort)
+# txt = reduce(lambda x,y: x + y + ', ', data_fil_sort_map, 'Cities: ')[0:-2]
+# print(txt)
+
+# func = lambda x: True if x.startswith('a') and x.endswith('a') else False
+# print(func('abcd'))
+# print(func('bcda'))
+# print(func('abcda'))
+# print(func('Abcd'))
+# print(func('bcdA'))
+# print(func('abcdA'))
+def func(string):
+    counter = 0
+    for i in string:
+        if i not in ('.0123456789'):
+            return False
+        if i == '.':
+            counter += 1
+    if float(i) >= 0 and counter < 2:
+        return True
+    else:
+        return False
+
+is_non_negative_num = lambda x: True if func(x) else False          # is_non_negative_num = lambda q: q.replace('.', '', 1).isdigit()
+print(is_non_negative_num('10.34ab'))
+print(is_non_negative_num('10.45'))
+print(is_non_negative_num('-18'))
+print(is_non_negative_num('-34.67'))
+print(is_non_negative_num('987'))
+print(is_non_negative_num('abcd'))
+print(is_non_negative_num('123.122.12'))
+print(is_non_negative_num('123.122'))
