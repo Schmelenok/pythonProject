842e5c1c489d8a8a0f62eee5d47fa5320bebfe07
Shmel <shmel_bush@tut.by>
2022-03-19 15:14:39 +0100

    hexlet

--- main.py
+++ main.py
@@ -64,3 +64,64 @@
 #         return f'https://{text}'
 #
 # print(normalize_url('АДРЕС'))
+
+# def join_numbers_from_range(start, finish):
+#     i = start
+#     string = ''
+#     while i <= finish:
+#         string += str(i)
+#         i += 1
+#     return string
+#
+# print(join_numbers_from_range(5,10))
+
+# def encrypt(text):
+#     i = 0
+#     encrypted_text = ''
+#     if len(text) % 2 != 0:
+#         end = text[-1]
+#         text = text[:(len(text)-1)]
+#     else:                                                       # nextChar = "" if (i + 1 >= len(text)) else text[i + 1]
+#                                                                     # result = result + nextChar + text[i]
+#         end = ''
+#     while i < len(text):
+#         encrypted_text += text[i+1] + text[i]
+#         i += 2
+#     return encrypted_text + end
+#
+# print(encrypt("move"))
+
+# def filter_string(text, char):
+#     new_text = ''
+#     for i in text:
+#         if i.lower() == char.lower():
+#             i = ''
+#         new_text += i
+#     return new_text
+#
+# print(filter_string('I look back if you are lost', 'o'))
+
+# def is_palindrome(text):
+#     new_text = ''
+#     for i in text:
+#         new_text = i + new_text
+#     return text == new_text
+#
+# print(is_palindrome('radar'))
+# print(is_palindrome('abs'))
+
+# from symbols import is_vowel
+# def count_vowels(string):
+#     counter = 0
+#     for i in string:
+#         if i.is_vowel():
+#             counter += 1
+#     return counter
+
+# BEGIN (write your solution here)
+# def sort_pair(numbers):
+#     num1, num2 = (numbers)
+#     return (num1, num2) if num2 >= num1 else (num2, num1)
+# # END
+# print(sort_pair((5, 1)))
+# print(sort_pair((7, 8)))
