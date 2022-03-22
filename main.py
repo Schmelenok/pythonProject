728e672b18bb7e8646ac5d1018ae64df2212a85d
Shmel <shmel_bush@tut.by>
2022-03-22 17:04:39 +0100

    hexlet

--- main.py
+++ main.py
@@ -125,3 +125,37 @@
 # # END
 # print(sort_pair((5, 1)))
 # print(sort_pair((7, 8)))
+
+# class MoneyBox:
+#     def __init__(self, capacity, val=0):
+#         self.capacity = capacity
+#         self.val = val
+#     def can_add(self, v):
+#         return self.val + v <= self.capacity
+#     def add(self, v):
+#         self.val += v
+
+class Buffer:
+    def __init__(self):
+        # конструктор без аргументов
+        self.l = list()
+
+    def add(self, *a):
+        # добавить следующую часть последовательности
+
+        for i in a:
+            l.append()
+
+    def get_current_part(self):
+        # вернуть сохраненные в текущий момент элементы последовательности в порядке, в котором они были добавлены
+        return self.l[:4]
+
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part() # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+buf.get_current_part() # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+buf.get_current_part() # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+buf.get_current_part() # вернуть [1]
