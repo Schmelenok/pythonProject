8e3ec0332e0fe10bfb1955a6a1c0dee5a9c9cc38
Shmel <shmel_bush@tut.by>
2022-03-24 21:01:22 +0100

    hexlet

--- main.py
+++ main.py
@@ -214,5 +214,31 @@
 # except (ArithmeticError, AssertionError) as error:
 #     print(type(error).__name__)
 
-a = 10
-print(get(a))
+class Buffer:
+    def __init__(self):
+        # конструктор без аргументов
+        self.lst = list()
+
+
+    def add(self, *a):
+        # добавить следующую часть последовательности
+        [self.lst.append(i) for i in a]
+        while len(self.lst) >= 5:
+            print(sum(self.lst[0:5]))
+            self.lst = self.lst[5:]
+
+
+    def get_current_part(self):
+        # вернуть сохраненные в текущий момент элементы последовательности в порядке, в котором они были добавлены
+        print(self.lst)
+        return self.lst
+
+buf = Buffer()
+buf.add(1, 2, 3)
+buf.get_current_part() # вернуть [1, 2, 3]
+buf.add(4, 5, 6) # print(15) – вывод суммы первой пятерки элементов
+buf.get_current_part() # вернуть [6]
+buf.add(7, 8, 9, 10) # print(40) – вывод суммы второй пятерки элементов
+buf.get_current_part() # вернуть []
+buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) # print(5), print(5) – вывод сумм третьей и четвертой пятерки
+buf.get_current_part() # вернуть [1]
